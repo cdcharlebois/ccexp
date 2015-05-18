a05e3bf9201b41a096f22856a4b99752438d60f3
Conner Charlebois <cdcharlebois@gmail.com>
2015-05-18 19:28:40 -0400

    added create form for new rounds

--- routes/golf.js
+++ routes/golf.js
@@ -1,11 +1,39 @@
 var express = require('express');
-//var db = require('mongoskin').db('mongodb://localhost:27017/ccexp');
+var db = require('mongoskin').db('mongodb://localhost:27017/ccexp');
 //mongodb://user:pass@host:port/dbname
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.send("This is the golf homepage :)");  
+	var rounds;
+	db.collection('rounds').find().toArray(function(err, result){
+		rounds = (!err) ? result : []; 
+//		console.log(rounds);
+		res.render('rounds', {r: rounds} );
+	});  
+//	console.log('outside the query: ');
+//	console.log(rounds);
+});
+
+router.get('/api', function(req, res, next) {
+	db.collection('rounds').find().toArray(function(err, result){
+		 if (!err) res.send(result);
+	}); 
+});
+
+router.get('/create', function(req, res, next) {
+	res.render('rounds/create');
+});
+router.post('/', function(req, res, next) {
+	console.log(req.body);
+	var newRound = {
+		score : req.body.score,
+		course : req.body.course
+	};
+	//console.log(newRound);
+	db.collection('rounds').insert(newRound, function(err, result) {
+		res.redirect('/golf');
+	});
 });
 
 module.exports = router;
