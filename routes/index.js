f7360817adf5d46aacc3aead489a4e42dc7d2e5d
conner <cdcharlebois@gmail.com>
2015-05-21 07:41:49 -0400

    updated index.jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express',
+    // social sharing
+    s: [
+      {
+        name: "Twitter",
+        link: "https://www.twitter.com/_connerc"
+      },
+      {
+        name: "Github",
+        link: "https://www.github.com/cdcharlebois"
+      }
+    ]
+  });
 });
 
 module.exports = router;
